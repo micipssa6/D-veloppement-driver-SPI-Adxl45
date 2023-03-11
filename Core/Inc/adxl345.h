dead9ad950b6b150f0df9ea35b73bb9a1bcd0563
Khalbenzaid <khaledbenzaidi38@gmail.com>
2023-03-12 00:28:50 +0100

    Ajout du driver spi et adxl345

--- Core/Inc/adxl345.h
+++ Core/Inc/adxl345.h
@@ -2,12 +2,31 @@
  * adxl345.h
  *
  *  Created on: Mar 11, 2023
- *      Author: Lenovo
+ *      Author: Khaled
  */
 
 #ifndef INC_ADXL345_H_
 #define INC_ADXL345_H_
+#include <stdint.h>
 
+// Definition des registres qui contiennent les données
 
+#define ADXL345_DATAX0 0x32
+#define ADXL345_DATAX1 0x33
+#define ADXL345_DATAY0 0x34
+#define ADXL345_DATAY1 0x35
+#define ADXL345_DATAZ0 0x36
+#define ADXL345_DATAZ1 0x37
+
+// Fonctions d'initialisation et de lecture des données
+// Fonction qui initialise le capteur
+void ADXL_Init(void);
+// Fonction pour lire les accéleration
+void ADXL_ReadAccel(int16_t* accel_data);
 
 #endif /* INC_ADXL345_H_ */
+
+
+
+
+
--- Core/Src/adxl345.c
+++ Core/Src/adxl345.c
@@ -2,7 +2,47 @@
  * adxl345.c
  *
  *  Created on: Mar 11, 2023
- *      Author: Lenovo
+ *      Author: Khaled
  */
 
+#include "adxl345.h"
+#include <stdint.h>
+#include "stm32f4xx_hal_spi.h"
 
+void ADXL_Init(SPI_HandleTypeDef* hspi)
+{
+  uint8_t tx_buffer[2] = {0}; // Buffer pour la trame d'initialisation
+
+  // Activer la mesure en écrivant la valeur 0x08 dans le registre POWER_CTL
+  tx_buffer[0] = 0x2D;
+  tx_buffer[1] = 0x08;
+  HAL_GPIO_WritePin(GPIOE, GPIO_PIN_3, GPIO_PIN_RESET);   // Activer le CS
+  HAL_SPI_Transmit(hspi, tx_buffer, 2, 100);               // Envoyer la trame
+  HAL_GPIO_WritePin(GPIOE, GPIO_PIN_3, GPIO_PIN_SET);     // Désactiver le CS
+
+  // Configurer le registre DATA_FORMAT pour avoir une résolution de 4mg/LSB et une plage de ±2g
+  tx_buffer[0] = 0x31;
+  tx_buffer[1] = 0x08;
+  HAL_GPIO_WritePin(GPIOE, GPIO_PIN_3, GPIO_PIN_RESET);   // Activer le CS
+  HAL_SPI_Transmit(hspi, tx_buffer, 2, 100);               // Envoyer la trame
+  HAL_GPIO_WritePin(GPIOE, GPIO_PIN_3, GPIO_PIN_SET);     // Désactiver le CS
+}
+
+void ADXL_ReadAccel(SPI_HandleTypeDef* hspi, int16_t* accel_data)
+{
+  uint8_t tx_buffer[7] = {0};  // Buffer pour la trame de lecture
+  uint8_t rx_buffer[7] = {0};  // Buffer pour la réception de la trame
+
+  // Configurer le premier octet (0x32) pour lire les données des 3 axes
+  tx_buffer[0] = ADXL345_DATAX0 | (1 << 7) | (1 << 6) | (1 << 5);
+
+  // Envoyer la trame de lecture
+  HAL_GPIO_WritePin(GPIOE, GPIO_PIN_3, GPIO_PIN_RESET);   // Activer le CS
+  HAL_SPI_TransmitReceive(hspi, tx_buffer, rx_buffer, 7, 100); // Envoyer et recevoir la trame
+  HAL_GPIO_WritePin(GPIOE, GPIO_PIN_3, GPIO_PIN_SET);     // Désactiver le CS
+  // Les données des axes X, Y et Z sont stockées dans les octets 1 à 6 de la trame reçue
+  // Les octets sont stockés en little-endian, donc il faut inverser l'ordre des octets pour obtenir la valeur correcte
+  accel_data[0] = (rx_buffer[1] << 8) | rx_buffer[0];
+  accel_data[1] = (rx_buffer[3] << 8) | rx_buffer[2];
+  accel_data[2] = (rx_buffer[5] << 8) | rx_buffer[4];
+ }
